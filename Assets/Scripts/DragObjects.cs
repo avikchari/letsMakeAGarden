58881b2721b3a7264c891704ed1520b6b864e8e3
Avik Chari <60371063+avikchari@users.noreply.github.com>
2021-01-31 08:19:05 +0800

    Added Mouse Drag Script and placed it onto Seed 1

---
+++ Assets/Scripts/DragObjects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObjects : MonoBehaviour
+{
+    private Vector3 offset; 
+    private float ZCoord;
+    
+    void OnMouseDown()
+    {
+        zCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        offset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = ZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+    
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() = offset;
+    }
+}
